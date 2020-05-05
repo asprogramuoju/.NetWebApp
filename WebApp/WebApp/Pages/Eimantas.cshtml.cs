9ebde9ede8151ee2ee1dd26bc6dacebc82c4711b
asprogramuoju <jeronimasklevinskas@gmail.com>
2020-05-05 22:02:21 +0300

    Mememe

---
+++ WebApp/WebApp/Pages/Eimantas.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class EimantasModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Midse mirs 2021 04 22.";
+        }
+    }
+}
